a60705f15246a1d50eabc9b340506ad38bd9f603
Matt Luongo <mhluongo@gmail.com>
2025-06-24 17:50:47 -0400

    Expand metadata around fractional units

--- src/types.ts
+++ src/types.ts
@@ -7,15 +7,25 @@ export type Asset = {
   name: string
 }
 
-// TODO where is the quantum display name?
-
 export type FungibleAsset = Asset & {
   // the commonly used code of an asset, eg "USD" or "BTC"
   code: string
   // the number of decimals needed to represent the smallest commonly
-  // transferrable "quantum" of a fungible asset. Eg, 2 for USD ($0.01)
+  // transferrable fractional unit of a fungible asset. Eg, 2 for USD ($0.01)
   // or 10 for BTC (0.0000000001)
   decimals: bigint
+  // a name for the smallest transferrable, fraction unit of a fungible asset.
+  // Eg "cent" for USD or "satoshi" for BTC.
+  //
+  // If multiple strings are provided, the additional strings are aliases for
+  // the first, which is the official name of the unit. Eg ["satoshi", "sat"]
+  // for BTC.
+  //
+  // If a record is provided, the number key is the number of decimals, and
+  // the string or strings are the names of the unit.
+  //
+  // Eg { 8: ["satoshi", "sat"], 12: ["millisatoshi", "millisat"] } for BTC.
+  fractionalUnit?: string | string[] | Record<number, string | string[]>
 }
 
 export type Currency = FungibleAsset & {
